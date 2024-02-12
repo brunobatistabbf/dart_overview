3defc5a33d61c5382c04b436e75f33d0c6e1db1e
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2024-02-12 15:57:02 -0300

    exercise path

---
+++ exercicios/exercicio1.dart
@@ -0,0 +1,19 @@
+//1. Dada uma palavra, calcule a pontuação Scabble desta palavra. Leve em consideração o valor de cada letra conforme a tabela seguir: Letras
+//Valor
+//A, E, I, O, U, L, N, R, S, T
+//D, G
+//B, C, M, P
+//F, H, V, W, Y
+//K
+//J, X
+//Q, Z
+
+//Exemplo: Banana deve ser pontuado:
+//B = 3 pontos
+//A = 1 ponto / cada
+//N = 1 pontos
+//Resultado = 3 + (1*3) + (1*2) = 8 pontos
+
+void main(List<String> args) {
+  print("Hello World!");
+}
